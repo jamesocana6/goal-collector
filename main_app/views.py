91f85145cdeae61744d29329344c17a80d0962e3
James Ocana <jamesocana@jamess-air.myfiosgateway.com>
2022-10-17 14:13:59 -0400

    dream index page created and connected

--- main_app/views.py
+++ main_app/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+class Dream: 
+    def __init__(self, name, description, steps, timeline = float("inf")): 
+        self.name = name
+        self.description = description
+        self.steps = steps
+        self.timeline = timeline
+
+dreams = [
+    Dream("Get a job", "get a job after bootcamp", "apply to at least 5 positions everyday", 3),
+    Dream("Graduate bootcamp", "2 more projects!", "go to class, do your work", 1),
+]
+
+def dreams_index(request):
+    return render(request, "dreams/index.html", {"dreams": dreams})
